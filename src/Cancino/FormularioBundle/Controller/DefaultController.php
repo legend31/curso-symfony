850b2d8410161e866859043bf01294c93ee96ab7
legend31 <fer143monster@gmail.com>
2015-08-27 15:14:39 -0600

    1er Commment rama1

--- src/Cancino/FormularioBundle/Controller/DefaultController.php
+++ src/Cancino/FormularioBundle/Controller/DefaultController.php
@@ -11,7 +11,7 @@ use Cancino\FormularioBundle\Entity\ProdForm;
 use Cancino\FormularioBundle\Form\ProdFormType;
 use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
 //Este es un comment
-
+//Le agrege otro coment a la rama1
 class DefaultController extends Controller
 {
     public function indexAction($name)
